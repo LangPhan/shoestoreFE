850ce4aa0bf6511094fd07ad54f6119f1151ac53
hoaBui <hoa199297@gmail.com>
2024-06-20 14:46:01 +0700

    Build base layout of product detail page

---
+++ src/pages/ProductDetailPage.jsx
@@ -0,0 +1,13 @@
+import Banner from "@/components/Products/Banner";
+import React, { Fragment } from "react";
+
+const ProductDetailPage = () => {
+  return (
+    <Fragment>
+      <Banner />
+      <section className="">This is product detail section</section>
+    </Fragment>
+  );
+};
+
+export default ProductDetailPage;
--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -2,28 +2,22 @@ import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import {
-  ToastContainer,
-  toast,
-} from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const RootPage = () => {
   return (
     //Root Layout config
-    <ThemeProvider
-      defaultTheme="dark"
-      storageKey="vite-ui-theme"
-    >
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <>
         <Header />
-        <button
+        {/* <button
           onClick={() =>
             toast.success("Hello")
           }
         >
           SHow toast
-        </button>
+        </button> */}
         <main>
           <Outlet />
         </main>
