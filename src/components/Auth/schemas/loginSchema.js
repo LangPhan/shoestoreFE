87f80cbb3e1811c4c643228498f0e3dcdc4e5d9c
hoaBui <hoa199297@gmail.com>
2024-07-22 10:07:39 +0700

    Resolve merge conflict

--- src/components/Auth/schemas/loginSchema.js
+++ src/components/Auth/schemas/loginSchema.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const loginSchema = z.object({
   username: z.string().min(1, "Username is required"),
-  password: z.string().min(1, "Password must be least 8 characters"),
+  password: z.string().min(8, "Password must be least 8 characters"),
 });
 
 export default loginSchema;
